378885558e56f90cea58ba7ca1e03cf5fd7b045b
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-02-09 00:35:25 +0530

    Added input validation

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator');
-const { error } = require('console');
 
 const app = express();
 
